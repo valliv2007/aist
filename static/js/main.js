25935c058e29931d855a415fe0faa22d4a4e5974
Varvara Korableva <v.korableva1990@yandex.ru>
2023-02-10 23:25:22 +0300

    added fonts, changed css

--- static/js/main.js
+++ static/js/main.js
@@ -9,3 +9,36 @@ document.addEventListener('click', function(event) {
       e.classList.remove('open');
   }
 });
+
+
+export default class Popup {
+    constructor(popupSelector){
+      this._popup = document.querySelector(popupSelector);
+      //this._handleEscClose = this._handleEscClose.bind(this);
+    }
+  
+    open() {
+      this._popup.classList.add('popup_opened');
+      document.addEventListener('keydown', this._handleEscClose);
+    };
+  
+    close() {
+      this._popup.classList.remove('popup_opened');
+      document.removeEventListener('keydown', this._handleEscClose);
+    };
+  
+    _handleEscClose = (evt) => {
+      if (evt.key === 'Escape') {
+        this.close();
+      };
+    };
+  
+    setEventListeners() {
+      this._popup.addEventListener('click', (evt) => {
+        if (evt.target.classList.contains('popup_opened') ||
+        evt.target.classList.contains('popup__close-button')) {
+          this.close();
+        };
+      });
+    };
+  }
