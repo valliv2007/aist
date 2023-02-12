98503a582dad55d4c884ecbda5e219d883221b70
Илья Снежко <valliv2007@ya.ru>
2023-02-12 19:59:27 +0200

    add filter in admin, cnhange redirect in carts

--- cart/urls.py
+++ cart/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 
 from . views import (
-    cart_add,
+    cart_add, cart_add_in_cart,
     cart_detail,
     cart_remove
 )
@@ -13,5 +13,6 @@ app_name = 'cart'
 urlpatterns = [
     path('', cart_detail, name='cart_detail'),
     path('add/<int:product_id>/', cart_add, name='cart_add'),
+    path('add_cart/<int:product_id>/', cart_add_in_cart, name='cart_add_in_cart'),
     path('remove/<int:product_id>/', cart_remove, name='cart_remove'),
 ]
