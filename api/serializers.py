1689d044a04516a267fbda5408221a57903ddf07
Илья Снежко <valliv2007@ya.ru>
2023-03-29 23:34:23 +0300

    add subcategories endpoint

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from lenivastore.models import Product
+from lenivastore.models import Product, Subcategory
 
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -9,4 +9,12 @@ class ProductSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Product
+        fields = ('__all__')
+
+
+class SubcategorySerializer(serializers.ModelSerializer):
+    category = serializers.ReadOnlyField(source='category.slug')
+
+    class Meta:
+        model = Subcategory
         fields = ('__all__')
