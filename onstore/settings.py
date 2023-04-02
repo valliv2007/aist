16f8e7e888e1bf3d28ab778e837c3c92cca13b1e
Илья Снежко <valliv2007@ya.ru>
2023-04-02 16:04:19 +0300

    change settings

--- onstore/settings.py
+++ onstore/settings.py
@@ -84,7 +84,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'onstore.wsgi.application'
 
-if DEBUG:
+if not DEBUG:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
