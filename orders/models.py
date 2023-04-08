31d7ffa966c367dc412b4ce925bf1da221cffc89
Илья Снежко <valliv2007@ya.ru>
2023-04-08 11:19:03 +0300

    added telegram and mail sent in ordersitems endpoint

--- orders/models.py
+++ orders/models.py
@@ -32,16 +32,19 @@ class Order(models.Model):
 
     def __str__(self):
         return 'Заказ: {}'.format(self.id)
-
+    """
     def get_total_cost(self):
         total_cost = sum(item.get_cost() for item in self.items.all())
         return total_cost - total_cost * (self.discount / Decimal('100'))
+   """
 
     def get_total_cost_no_sale(self):
         return sum(item.get_cost() for item in self.items.all())
-
+    
+    
     def get_order_discount(self):
-        return self.get_total_cost_no_sale() - self.get_total_cost()
+        return self.get_total_cost_no_sale() - self.order_price
+
 
 
 class OrderItem(models.Model):
@@ -49,7 +52,7 @@ class OrderItem(models.Model):
                               on_delete=models.CASCADE)
     product = models.ForeignKey(Product, related_name='order_items',
                                 on_delete=models.CASCADE)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.PositiveIntegerField()
     quantity = models.PositiveIntegerField(default=1)
 
     def __str__(self):
--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-
+from django.conf import settings
 from django.template.loader import render_to_string
 from django.urls import reverse
 from django.http import HttpResponse
@@ -32,10 +32,7 @@ def order_create(request):
                                          quantity=item['quantity'])
             cart.clear()
             request.session['order_id'] = order.id
-            if order.card_paid:
-                return redirect(reverse('payment:process'))
-            else:
-                return offline_paid(request, cart)
+            return offline_paid(request, cart)
     else:
         if request.user.is_authenticated:
             form = OrderCreateForm(instance=request.user)
